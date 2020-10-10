16952cc676126e7bd2acdc6f11883da4c51d0846
Denis <densPro09.07.95@gmail.com>
2020-10-10 15:42:01 -0400

    convert catalog

--- src/app/products/product-data.ts
+++ src/app/products/product-data.ts
@@ -5,38 +5,38 @@ export class ProductData {
   static products: Product[] = [
     {
       id: 1,
-      name: 'Trodat 4911',
-      description: 'standart stamp',
+      name: 'Puppy food',
+      description: 'Comida creada ori veterinarios y nutricionistas.',
       price: 19.95,
-      imageFileName: 'trodat_4911.PNG'
+      imageFileName: 'dog-food.PNG'
     },
     {
       id: 2,
-      name: 'Trodat 4911',
-      description: 'standart stamp',
-      price: 32.99,
-      imageFileName: 'trodat_4911.PNG'
+      name: 'Puppy food',
+      description: 'Comida creada ori veterinarios y nutricionistas.',
+      price: 19.95,
+      imageFileName: 'dog-food.PNG'
     },
     {
       id: 5,
-      name: 'Trodat 4911',
-      description: 'standart stamp',
-      price: 8.9,
-      imageFileName: 'trodat_4911.PNG'
+      name: 'Puppy food',
+      description: 'Comida creada ori veterinarios y nutricionistas.',
+      price: 19.95,
+      imageFileName: 'dog-food.PNG'
     },
     {
       id: 8,
-      name: 'Trodat 4911',
-      description: 'standart stamp',
-      price: 11.55,
-      imageFileName: 'trodat_4911.PNG'
+      name: 'Puppy food',
+      description: 'Comida creada ori veterinarios y nutricionistas.',
+      price: 19.95,
+      imageFileName: 'dog-food.PNG'
     },
     {
       id: 10,
-      name: 'Trodat 4911',
-      description: 'Standard two-button video game controller',
-      price: 35.95,
-      imageFileName: 'trodat_4911.PNG'
+      name: 'Puppy food',
+      description: 'Comida creada ori veterinarios y nutricionistas.',
+      price: 19.95,
+      imageFileName: 'dog-food.PNG'
     }
   ];
 }
